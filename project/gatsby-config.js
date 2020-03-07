21818753aa528ed567cdd50ba9c90bf4f8845994
Adam Peterka <peterkaa@gmatc.matc.edu>
2020-03-07 10:35:06 -0600

    added env file

--- project/gatsby-config.js
+++ project/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Project 1`,
@@ -8,8 +12,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `wkstnc50lzn8`,
-        accessToken: `9QxqEu-M6oz0xw8-oMfHVU1Rpd2A7nn5qbRnkz79rpA`,
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`,
       },
     },
     `gatsby-plugin-react-helmet`,
